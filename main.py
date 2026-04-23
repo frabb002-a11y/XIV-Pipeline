15447a46aad52b2a548f37cef484f5a58fcba376
frabb002-a11y <f.rabb002@gmail.com>
2026-04-23 10:12:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import requests
 import pandas as pd
-from sqlalchemy import engine
+from sqlalchemy import create_engine, text
+
 response = requests.get('https://universalis.app/api/v2/extra/stats/least-recently-updated?world=louisoix')
 data = response.json()
 d_items = data['items']
 
 cleaned = {
-        'itemID': [],
-        'lastUploadTime': [],
-        'worldID': [], 
-        'worldName': []
-    } 
+    'itemID': [],
+    'lastUploadTime': [],
+    'worldID': [],
+    'worldName': []
+}
 
-for item in d_items :
+for item in d_items:
     cleaned['itemID'].append(item['itemID'])
     cleaned['lastUploadTime'].append(item['lastUploadTime'])
     cleaned['worldID'].append(item['worldID'])
     cleaned['worldName'].append(item['worldName'])
 
- # so ive extracted, ive transformed now i need to load. but before that, why not turn it into a datatable?
+print(d_items)
 
-df_cleaned = pd.DataFrame(cleaned)
+engine = create_engine("dialect+driver://username:password@host:port/database",echo=True) # creates an Engine object that manages connections., now i need to know how to execute the connection where it sends the imported and cleaned "data" to the DB
 
-print(df_cleaned)
+# question is, how do i go about doing this, i think i need to run the connection
 
+with engine.connect() as connection:  # this can either execute SQL or insert data, how does it insert data
+# what are with statements anyway?
+
+insert = text(
+    """
+
+    """
+)  # text function allows one to write SQL. This is the SQL you will write.
+connection.execute(insert, d_items) # this should hopefully have us send the data over. we will stop here
+
+# print(engine)
