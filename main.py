d366e9e4d9f7971894ccae6ab23bf2fecc138e41
frabb002-a11y <f.rabb002@gmail.com>
2026-04-25 16:07:16 +0100

    Enhance main.py to convert lastUploadTime to a human-readable format and add datetime imports for improved timestamp handling

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ for item in d_items:
 engine = create_engine("postgresql://neondb_owner:npg_3R2XoTSwUrtD@ep-billowing-boat-abutsytw-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 # ive made the engine make a connection, now i need to load the sql into the database.
-
+"""
 load_d_items = text (
     "insert into "
 )
-# before i do that i need to create a sql script to put into my DB. i wonder if there is a way for me to load the sql scipt from python into the DB. im sure it is possible 
+"""
+
+# before i do that i need to create a sql script to put into my DB.
+#  i wonder if there is a way for me to load the sql scipt from python into the DB. im sure it is possible 
+
