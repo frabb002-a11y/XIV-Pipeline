da4438d3c87b040cbbea7f67444270da01864882
frabb002-a11y <f.rabb002@gmail.com>
2026-04-28 15:40:47 +0100

    Update main.py to fetch most recently updated items, add itemname to cleaned data, and modify database schema for itemid type. Implement MERGE statement for data insertion to handle updates and new entries efficiently.

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from sqlalchemy import create_engine, text
 
 # Imports required for the script
 
-response = requests.get('https://universalis.app/api/v2/extra/stats/least-recently-updated?world=louisoix')
+response = requests.get('https://universalis.app/api/v2/extra/stats/most-recently-updated?world=louisoix')
 data = response.json()
 d_items = data['items']
-
+# print(d_items)
 # gets recently updated infromation from the api and stores it in d_items
 
 cleaned = {
     'itemid': [],
+    'itemname': [],
     'lastuploadtime': [],
     'worldid': [],
     'worldname': []
@@ -32,10 +33,7 @@ for item in d_items:
 
 # Primary data cleaning pass, corrects lastuploadtime format
 
-print(cleaned)
-
 url = "https://v2.xivapi.com/api/sheet/Item/"
-# print(cleaned)
 
 # Database which has item name for the corresponding itemid
 
@@ -43,24 +41,24 @@ for i, item in enumerate[Any](cleaned['itemid']) : # why would i class things? l
     n_url = f"{url}{item}?fields=Name"
     response = requests.get(n_url)
     xiv_data = response.json()
-    cleaned['itemid'][i] = xiv_data['fields']['Name']
-  
+    cleaned['itemname'].append(xiv_data['fields']['Name'])
+
  # takes a long time to execute. i should find a way to execute all urls all at once.
  # im thinking, build the url list and then execute the list like that.
  # loops through each item in the itemid list and replaces itemid with their proper names
 
+# print(cleaned)
 
 # commented to reduce loading time for now
 # print(cleaned['worldid'])
 
 df_cleaned = pd.DataFrame(cleaned)
-print(df_cleaned)
-
+# print(df_cleaned)
 
 # When turning it into a dataframe WORLDID dissapears. leave for later                     
 # Data has been cleaned. time to load the data into PostGres
 
-engine = create_engine("postgresql://neondb_owner:npg_3R2XoTSwUrtD@ep-billowing-boat-abutsytw-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",echo = True)
+engine = create_engine("postgresql://neondb_owner:npg_3R2XoTSwUrtD@ep-billowing-boat-abutsytw-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",echo=False)
 
 # creates engine which can connect to the DB.
 
@@ -69,30 +67,38 @@ create_db = text (
 ;
 
 CREATE TABLE IF NOT EXISTS xiv_data.raw_data (
-    itemid VARCHAR(50) PRIMARY KEY,
+    itemid INT PRIMARY KEY,
+    itemname VARCHAR(50),
     lastuploadtime VARCHAR(50),
     worldid INTEGER,
     worldname VARCHAR(50) 
 );"""
 )
-
 # Creates DB Schema and table
+
+rows = df_cleaned.to_dict("records")
+# print(rows)
+
+insert_data = text (
+  """ MERGE INTO xiv_data.raw_data t
+    USING (
+    VALUES (:itemid, :itemname, :lastuploadtime, :worldid, :worldname)
+    ) AS s (itemid, itemname, lastuploadtime, worldid, worldname)
+ON t.itemid = s.itemid
+
+WHEN MATCHED THEN
+    UPDATE SET
+        lastuploadtime = s.lastuploadtime,
+        worldid = s.worldid,
+        worldname = s.worldname
+
+WHEN NOT MATCHED THEN
+    INSERT (itemid, itemname, lastuploadtime, worldid, worldname)
+    VALUES (s.itemid, s.itemname, s.lastuploadtime, s.worldid, s.worldname);
+""" 
+)
 
 with engine.begin() as conn:
     conn.execute(create_db)
-
-# Creates DB in postgres if it does not exist already.
-
-# now i need to insert the data into the datatables
-
-df_cleaned.to_sql( 
-    name="raw_data",
-    con=engine, 
-    schema="xiv_data", 
-    if_exists="append", 
-    index=False )
-
-# turn them into def functions to all be called by def main(). 
-# make it so that it can run more than once # create a scheduler 
-# for eaach def create a failure safeguard and true failure output outputting what caused the failure
-# 30 minutes practicing SQL everyday
+    conn.execute(insert_data, rows)
+   
