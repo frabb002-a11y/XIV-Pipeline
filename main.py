b8bd8def8b4824d928c751b4d88d9cfaaa472361
frabb002-a11y <f.rabb002@gmail.com>
2026-04-29 12:20:37 +0100

    Refactor main.py to implement concurrent fetching of item names using ThreadPoolExecutor, improving performance. Cleaned up data handling and updated comments for clarity. Adjusted database schema creation and insertion logic for better organization.

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from typing import Any
 import requests
+import time
 from datetime import datetime
 import pandas as pd
 from sqlalchemy import create_engine, text
+from concurrent.futures import ThreadPoolExecutor
 
 # Imports required for the script
 
 response = requests.get('https://universalis.app/api/v2/extra/stats/most-recently-updated?world=louisoix')
 data = response.json()
 d_items = data['items']
-# print(d_items)
 # gets recently updated infromation from the api and stores it in d_items
 
 cleaned = {
     'itemid': [],
-    'itemname': [],
     'lastuploadtime': [],
     'worldid': [],
-    'worldname': []
+    'worldname' : []
 }
 
+ma,es
 # Creates dictionary for the cleaned data to be held.
 
 for item in d_items:
@@ -30,31 +30,34 @@ for item in d_items:
         )
     cleaned['worldid'].append(item['worldID'])
     cleaned['worldname'].append(item['worldName'])
-
 # Primary data cleaning pass, corrects lastuploadtime format
 
 url = "https://v2.xivapi.com/api/sheet/Item/"
 
 # Database which has item name for the corresponding itemid
 
-for i, item in enumerate[Any](cleaned['itemid']) : # why would i class things? later ig.
-    n_url = f"{url}{item}?fields=Name"
-    response = requests.get(n_url)
-    xiv_data = response.json()
-    cleaned['itemname'].append(xiv_data['fields']['Name'])
+def fetch_name(item_id):
+    url = f"https://v2.xivapi.com/api/sheet/Item/{item_id}?fields=Name"
+    response = requests.get(url)
+    data = response.json()
+    print(response.status_code)
+    return data['fields']['Name']
 
- # takes a long time to execute. i should find a way to execute all urls all at once.
- # im thinking, build the url list and then execute the list like that.
- # loops through each item in the itemid list and replaces itemid with their proper names
+with ThreadPoolExecutor(len(cleaned['itemid'])) as executioner:
+    start = time.time()
+    names = list(executioner.map(fetch_name,cleaned['itemid']))
+    end = time.time()
+    execution = f"execution time: {round((end - start), 2)} seconds"
+    print(execution)
 
-# print(cleaned)
-
-# commented to reduce loading time for now
-# print(cleaned['worldid'])
+# completed async
 
 df_cleaned = pd.DataFrame(cleaned)
-# print(df_cleaned)
+df_names = pd.DataFrame(names)
 
+# create two schemas and then join
+
+"""
 # When turning it into a dataframe WORLDID dissapears. leave for later                     
 # Data has been cleaned. time to load the data into PostGres
 
@@ -63,7 +66,7 @@ engine = create_engine("postgresql://neondb_owner:npg_3R2XoTSwUrtD@ep-billowing-
 # creates engine which can connect to the DB.
 
 create_db = text (
-    """CREATE SCHEMA IF NOT EXISTS xiv_data
+   CREATE SCHEMA IF NOT EXISTS xiv_data
 ;
 
 CREATE TABLE IF NOT EXISTS xiv_data.raw_data (
@@ -72,7 +75,7 @@ CREATE TABLE IF NOT EXISTS xiv_data.raw_data (
     lastuploadtime VARCHAR(50),
     worldid INTEGER,
     worldname VARCHAR(50) 
-);"""
+);
 )
 # Creates DB Schema and table
 
@@ -80,7 +83,7 @@ rows = df_cleaned.to_dict("records")
 # print(rows)
 
 insert_data = text (
-  """ MERGE INTO xiv_data.raw_data t
+  MERGE INTO xiv_data.raw_data t
     USING (
     VALUES (:itemid, :itemname, :lastuploadtime, :worldid, :worldname)
     ) AS s (itemid, itemname, lastuploadtime, worldid, worldname)
@@ -95,10 +98,10 @@ WHEN MATCHED THEN
 WHEN NOT MATCHED THEN
     INSERT (itemid, itemname, lastuploadtime, worldid, worldname)
     VALUES (s.itemid, s.itemname, s.lastuploadtime, s.worldid, s.worldname);
-""" 
+
 )
 
 with engine.begin() as conn:
     conn.execute(create_db)
     conn.execute(insert_data, rows)
-   
+"""
