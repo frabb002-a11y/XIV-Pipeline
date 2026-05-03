9c57b01b552e8309eb41aae083c449fceaa4a7d9
frabb002-a11y <f.rabb002@gmail.com>
2026-05-03 13:59:15 +0100

    Refactor main.py to rename 'name' to 'itemname' in data structure, enhance database schema for name data, and implement logic for fetching and inserting item names concurrently. Update data handling and SQL statements for improved clarity and functionality.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ cleaned = {
 
 names = {
     'itemid': [],
-    'name' : [],
+    'itemname' : [],
 }
 # Creates dictionary for the cleaned data to be held.
 
@@ -38,33 +38,17 @@ for item in d_items:
     cleaned['worldname'].append(item['worldName'])
 
 names['itemid'] = cleaned['itemid']
+names['itemname'] = ''
 # Primary data cleaning pass, corrects lastuploadtime format
 
-url = "https://v2.xivapi.com/api/sheet/Item/"
 
 # Database which has item name for the corresponding itemid
-
-def fetch_name(item_id):
-    url = f"https://v2.xivapi.com/api/sheet/Item/{item_id}?fields=Name"
-    response = requests.get(url)
-    data = response.json()
-    print(response.status_code)
-    return data['fields']['Name']
-
-with ThreadPoolExecutor(20) as executioner:
-    start = time.time()
-    names['name'] = list(executioner.map(fetch_name,cleaned['itemid']))
-    end = time.time()
-    execution = f"execution time: {round((end - start), 2)} seconds"
-    print(execution)
 
 # completed async
 
 df_cleaned = pd.DataFrame(cleaned)
 df_names = pd.DataFrame(names)
 
-print(df_names)
-print(df_cleaned)
 # create a schema for names. you can either pregenerate all the names and their associated itemid and then merge it in the db to prevent need for calling.
 # or if the name does not exist in the database in the name schema then run a call to generate that name, populate the name schema and then merge
 
@@ -74,11 +58,13 @@ print(df_cleaned)
 
 # When turning it into a dataframe WORLDID dissapears. leave for later                     
 # Data has been cleaned. time to load the data into PostGres
+
+ECHO = False
 try:
-    engine = create_engine(os.getenv("DATABASE_URL"), echo=False)
+    engine = create_engine(os.getenv("DATABASE_URL"), echo=ECHO)
 except Exception as e:
     print("Cannot retrieve database url, Running Local mode:")
-    engine = create_engine(DATABASE_URL(), echo=False)
+    engine = create_engine(DATABASE_URL(), echo=ECHO)
 # creates engine which can connect to the DB.
 
 create_db = text ("""
@@ -92,18 +78,17 @@ CREATE TABLE IF NOT EXISTS xiv_data.raw_data (
     worldname VARCHAR(50) 
 );
 
-CREATE TABLE IF NOT EXISTS xiv_data.raw_names (
+CREATE TABLE IF NOT EXISTS xiv_data.name_data (
     itemid INT PRIMARY KEY,
-    itemname VARCHAR(50)
+    itemname VARCHAR(100)
 );
 """)
 # Creates DB Schema and two tabbles, raw data and name data.
 
 
-rows = df_cleaned.to_dict("records")
-print(rows)
+rows_data = df_cleaned.to_dict("records")
 
-insert_data = text ("""
+insert_raw_data = text ("""
   MERGE INTO xiv_data.raw_data t
     USING (
     VALUES (:itemid, :lastuploadtime, :worldid, :worldname)
@@ -119,20 +104,101 @@ WHEN MATCHED THEN
 WHEN NOT MATCHED THEN
     INSERT (itemid, lastuploadtime, worldid, worldname)
     VALUES (s.itemid, s.lastuploadtime, s.worldid, s.worldname);
-
-""")
 """
-extract_name_data = text (
-select *
-from xiv_data.raw_names
 )
-name_data = extract_name_data.json()
-print(name_data)
+
+rows_name = df_names.to_dict("records")
+
+insert_name_data = text("""
+    MERGE INTO xiv_data.name_data AS t
+    USING (
+        VALUES (:itemid)
+    ) AS s (itemid)
+    ON t.itemid = s.itemid
+
+    WHEN MATCHED THEN
+        UPDATE SET
+            itemid = s.itemid
+
+    WHEN NOT MATCHED THEN
+        INSERT (itemid)
+        VALUES (s.itemid);
+""")
+
+extract_name_data = text (
+    """
+select *
+from xiv_data.name_data
+where itemname IS NULL
+""")
+
+reset = text ( """
+DROP TABLE IF EXISTS xiv_data.name_data;
+DROP TABLE IF EXISTS xiv_data.raw_data;
 """
+)
+with engine.begin() as conn:
+    conn.execute(reset)
+    conn.execute(create_db)
+    conn.execute(insert_raw_data, rows_data)
+    conn.execute(insert_name_data, rows_name)
+    response = conn.execute(extract_name_data)
+    data = response.mappings().all()
+
+print(data)
+
+new_names = {
+    'itemid': [],
+    'itemname' : []
+}
+
+
+def fetch_name(itemid):
+    url = f"https://v2.xivapi.com/api/sheet/Item/{itemid}?fields=Name"
+    response = requests.get(url)
+    ddata = response.json()
+    return ddata['fields']['Name']
+
+
+for item in data:
+    new_names['itemid'].append(item['itemid'])
+
+with ThreadPoolExecutor(20) as executioner:
+    start = time.time()
+    new_names['itemname'] = list(executioner.map(fetch_name,new_names['itemid']))
+    end = time.time()
+    execution = f"execution time: {round((end - start), 2)} seconds"
+    print(execution)
+
+print(new_names)
+
+
+"""
+print(new_names)
+
+df_ddata = pd.DataFrame(data)
+
+
+rows_ddata = df_cleaned.to_dict("records")
+print(rows_ddata)
+insert_rows_ddata = text (
+    INSERT INTO xiv_data.name_data
+        (itemid, itemname)
+        VALUES (:itemid, :itemname);
+)
 
 with engine.begin() as conn:
-    conn.execute(create_db)
-    conn.execute(insert_data, rows)
+    conn.execute(insert_rows_ddata,rows_ddata)
+
+
+# upload all data to database, merge, insert, update
+# merge, insert or update just the nameid into namedata database
+# retrieve all namedata from database which has blank itemnames
+# iterate through those itemids to get all the namedata complete 
+# insert all those itemids into namedata 
+
 
 # the second is more complex. it needs python to retrieve data from the db and then see if the data exists, if not then it generates. i think while the second is more complicated it would be more intereesting. 
 # i completed a local running version, password is retrieved from password.py. i created a second datatable full of names. this will set us up for "filling" only if data exists in the DB.
+
+"""
